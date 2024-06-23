a06e5073f39f05eecb1dbe2ee70dc6a667f81814
John Mullaney <j.a.mullaney1@gmail.com>
2024-06-23 16:10:42 -0700

    Created animated sprite for player!

--- 2D_Platformer/Scripts/Player.gd
+++ 2D_Platformer/Scripts/Player.gd
@@ -5,6 +5,8 @@ var jump_foce : float = 200.0
 var gravity : float = 500.0
 
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -12,10 +14,15 @@ func _physics_process(delta):
 	velocity.x = 0
 	if Input.is_key_pressed(KEY_LEFT):
 		velocity.x -= move_speed
-	if Input.is_key_pressed(KEY_RIGHT):
+		_animated_sprite.play("run")
+	elif Input.is_key_pressed(KEY_RIGHT):
 		velocity.x += move_speed
+		_animated_sprite.play("run")
+	else:
+		_animated_sprite.stop()
 	
 	if (Input.is_key_pressed(KEY_SPACE) or Input.is_key_pressed(KEY_UP)) and is_on_floor():
+		_animated_sprite.stop()
 		velocity.y = -jump_foce
 	move_and_slide()
 
